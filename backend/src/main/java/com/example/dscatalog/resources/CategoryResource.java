a5fa73ed7e147fd28d5c8c300f2d334c9ef07586
rodrigo matsumoto <matsumoto.rodrigo1@gmail.com>
2025-08-19 22:32:52 -0300

    add findById method in CategoryService and corresponding endpoint in CategoryResource; implement exception handling for resource not found

--- backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
@@ -4,6 +4,7 @@ import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,10 @@ public class CategoryResource {
     return ResponseEntity.ok().body(list);
   }
   
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+    CategoryDTO dto = service.findById(id);
+    
+    return ResponseEntity.ok().body(dto);
+  }
 }
--- backend/src/main/java/com/example/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.example.dscatalog.services;
 import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.repositories.CategoryRepository;
+import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -22,6 +24,15 @@ public class CategoryService {
     List<Category> list = repository.findAll();
     
     return list.stream().map(CategoryDTO::new).toList();
+  }
+  
+  @Transactional(readOnly = true)
+  public CategoryDTO findById(Long id) {
+    Optional<Category> obj = repository.findById(id);
+    
+    Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+    
+    return new CategoryDTO(entity);
   }
   
 }
