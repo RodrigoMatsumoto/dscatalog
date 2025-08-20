190d37158d030967be1454a81860628e2cc7386b
rodrigo matsumoto <matsumoto.rodrigo1@gmail.com>
2025-08-19 23:47:10 -0300

    add delete method in CategoryService and corresponding DELETE endpoint in CategoryResource; enable category deletion with exception handling

--- backend/src/main/java/com/example/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.example.dscatalog.services;
 import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.repositories.CategoryRepository;
+import com.example.dscatalog.services.exceptions.DatabaseException;
 import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -57,6 +60,16 @@ public class CategoryService {
       return new CategoryDTO(entity);
     } catch (EntityNotFoundException e) {
       throw new ResourceNotFoundException("Id not found " + id);
+    }
+  }
+  
+  public void delete(Long id) {
+    try {
+      repository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException("Id not found " + id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException("Integrity violation");
     }
   }
   
