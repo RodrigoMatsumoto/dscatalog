0e0d9f9f5b616d9433f5242c813fc71094e4facf
rodrigo matsumoto <matsumoto.rodrigo1@gmail.com>
2025-08-19 22:41:30 -0300

    add insert method in CategoryService and corresponding POST endpoint in CategoryResource; enable category creation

--- backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
@@ -3,11 +3,10 @@ package com.example.dscatalog.resources;
 import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,4 +32,14 @@ public class CategoryResource {
     
     return ResponseEntity.ok().body(dto);
   }
+  
+  @PostMapping
+  public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+    dto = service.insert(dto);
+    
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+    
+    return ResponseEntity.created(uri).body(dto);
+  }
+  
 }
